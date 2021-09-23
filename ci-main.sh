579a22ac988a0fa28514768afd88a23760c8c28d
Jeremy Busk <jeremybusk@gmail.com>
2021-09-23 06:24:54 -0600

    add flake8 to runner

--- ci-main.sh
+++ ci-main.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 echo "Style check."
-flake8 app.py
+pip install flake8 && flake8 app.py
 
 echo "Build and push docker container to Dockerhub."
 echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
