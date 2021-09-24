29521525a4e60fd5b99f9c71ff642c919aba2b50
Jeremy Busk <jeremybusk@gmail.com>
2021-09-24 06:10:09 -0600

    update ci main

--- ci-main.sh
+++ ci-main.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -e
+
 echo "Style check."
 pip install flake8 && flake8 app.py
 
 echo "Build and push docker container to Dockerhub."
+release=latest
+repo=uvoo/sumo-collector-healer
+tag=$repo:${release}
 echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
-./buildPush.sh
+docker build --tag ${tag} .
+docker push ${tag}
 docker logout
